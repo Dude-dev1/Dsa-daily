b386013e1fb439ceb2284f7fd250b490b7d765c8
Dude-dev1 <stephenyiadomosei@gmail.com>
2026-01-01 18:28:22 +0000

    countNegatives DSA challenge...day 1

--- countNegatives.js
+++ countNegatives.js
@@ -2,17 +2,16 @@
 
 var countNegatives = function (grid) {
   let count = 0;
-  let n = grid.length;
-  let m = grid[0].length;
 
-  for (let col = 0; col < m; col++) {
-    for (let row = 0; row < n; row++) {
-      if (grid[col][row] < 0) {
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] < 0) {
         count++;
       }
     }
   }
   return count;
 };
+
 // console.log(countNegatives([[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]])); // 8
 // console.log(countNegatives([[3, 2], [1, 0]])); // 0
